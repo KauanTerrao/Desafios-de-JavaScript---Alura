69122000648dd5e296dfb304e5990b0a75997541
Kauan da Silva Terrão <kauanterrao2@outlook.com>
2025-09-21 12:36:55 -0300

    Inclusão e implementação da função exibirMensagem(), inclusão de novo estilo no CSS e atualização nos DocStrings

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
  * 
  * A função irá gerar valores pseudo-aleatorios através dos dados informados pelo usuário
  * 
- * Parâmetros: Não se aplica.
+ * PARÂMETROS: Não se aplica.
  * 
- * Retorno: Não há retorno.
+ * RETORNO: Não há RETORNO.
  */
 function sortear() {
-    var statusPodeSeguir = validarDados();
+    const statusPodeSeguir = validarDados();
 
     if (statusPodeSeguir) {
         const quantidade = parseInt(document.getElementById('quantidade').value);
@@ -24,8 +24,7 @@ function sortear() {
             sorteados.push(numero);
         }
 
-        const resultado = document.getElementById('resultado');
-        resultado.innerHTML = `<p class="texto__paragrafo">Números sorteados: ${sorteados.join(" - ")}</p>`;
+        exibirMensagem('texto__paragrafo', `Números sorteados: ${sorteados.join(" - ")}`);
         alterarStatusBotao();
     }
 }
@@ -34,13 +33,13 @@ function sortear() {
  * 
  * A função gera um número inteiro pseudo-aleatório dentro de um intervalo definido.
  *
- * Parâmetros:
+ * PARÂMETROS:
  * 
- * min — Valor mínimo do intervalo (inclusive). Ex: 1, 2, 3...
+ * @param {number} min - Valor mínimo do intervalo (inclusive). Ex: 1, 2, 3...
  * 
- * max — Valor máximo do intervalo (inclusive). Deve ser igual ou maior que "min".
+ * @param {number} max Valor máximo do intervalo (inclusive). Deve ser >= que "min".
  *
- * Retorno: Um número inteiro entre "min" e "max", incluindo ambos os extremos.
+ * @returns {number} Um número inteiro entre "min" e "max", incluindo ambos os extremos.
  */
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -52,9 +51,9 @@ function obterNumeroAleatorio(min, max) {
  * 
  * Sendo elas as classes: 'container__botao-desabilitado' e 'container__botao'.
  *
- * Parâmetros: Não se aplica.
+ * PARÂMETROS: Não se aplica.
  *
- * Retorno: Não há retorno.
+ * RETORNO: Não há retorno.
  */
 function alterarStatusBotao() {
     const botao = document.getElementById('btn-reiniciar');
@@ -69,15 +68,15 @@ function alterarStatusBotao() {
  * 
  * A função irá restaurar os campos e o texto informativo.
  *
- * Parâmetros: Não se aplica.
+ * PARÂMETROS: Não se aplica.
  *
- * Retorno: Não há retorno.
+ * RETORNO: Não há retorno.
  */
 function reiniciar() {
     document.getElementById('quantidade').value = '';
     document.getElementById('de').value = '';
     document.getElementById('ate').value = '';
-    document.getElementById('resultado').innerHTML = '<p class="texto__paragrafo">Números sorteados:  nenhum até agora</p>';
+    exibirMensagem('texto__paragrafo', 'Números sorteados:  nenhum até agora');
     alterarStatusBotao();
 }
 
@@ -86,29 +85,23 @@ function reiniciar() {
  * A função valida todos os campos necessários para o sorteio e verifica se é possível prosseguir.
  * 
  * Para cada verificação que falhar, exibe o aviso correspondente na tela.
- *
- * Parâmetros: Não se aplica.
- * 
- * Retorno: true se todos os campos estiverem válidos; false caso contrario.
+ *  
+ * @returns {boolean} true se todos os campos estiverem válidos; false caso contrario.
  */
 function validarDados() {
-    if (!validarCamposVazios()) return false;
-    if (!validarValorInteiroDosCampos()) return false;
-    if (!validarInversaoDosValores()) return false;
-    if (!validarQuantidadeXIntervaloDeSorteio()) return false;
-    return true;
+    return  validarCamposVazios() &&
+            validarValorInteiroDosCampos() &&
+            validarInversaoDosValores() &&
+            validarQuantidadeXIntervaloDeSorteio();
 }
-
 
 /** Descrição da função:
  * 
  * A função irá validar se todos os valores 'quantidade', 'de' e 'ate' foram informados,
  * 
- * Se algum valor não foi informado ele para o sorteio e informa um aviso na tela.
+ * Se algum valor não for informado ele irá parar o sorteio e informar um aviso na tela.
  *
- * Parâmetros: Não se aplica.
- *
- * Retorno: true se válido, false caso contrário
+ * @returns {boolean} true se válido, false caso contrário
  */
 function validarCamposVazios() {
     // Atribui os status dos campos:
@@ -118,29 +111,28 @@ function validarCamposVazios() {
     let statusCampoAteVazia = document.getElementById('ate').value == '';
 
     if (statusQuantidadeVazia) {
-        document.getElementById('resultado').innerHTML = '<p class="texto__paragrafo">Atenção: Favor informar a quantidade</p>';
+        exibirMensagem('erro', 'Atenção: Favor informar a quantidade');
         return false;
     }
     if (statusCampoDeVazia) {
-        document.getElementById('resultado').innerHTML = '<p class="texto__paragrafo">Atenção: Favor informar o valor inicial</p>';
+        exibirMensagem('erro', 'Atenção: Favor informar o valor inicial');
         return false;
     }
     if (statusCampoAteVazia) {
-        document.getElementById('resultado').innerHTML = '<p class="texto__paragrafo">Atenção: Favor informar o valor final</p>';
+        exibirMensagem('erro', 'Atenção: Favor informar o valor final');
         return false;
     }
 
     return true;
 }
+
 /** Descrição da função:
  * 
  * A função irá validar se todos os valores 'quantidade', 'de' e 'ate' são positivos e superior a 0,
  * 
- * Se algum valor for menor ou igual a 0 ele para o sorteio e informa um aviso na tela.
+ * Se algum valor for menor ou igual a 0 ele irá parar o sorteio e informar um aviso na tela.
  *
- * Parâmetros: Não se aplica.
- *
- * Retorno: true se válido, false caso contrário
+ * @returns {boolean} true se válido, false caso contrário
  */
 function validarValorInteiroDosCampos() {
     // Atribui os status dos campos:
@@ -150,17 +142,17 @@ function validarValorInteiroDosCampos() {
     let valorAteEhPermitido = document.getElementById('ate').value < 1;
 
     if (valorQuantidadeEhPermitido) {
-        alert('O valor do campo "quantidade" não pode ser 0 ou negativo, insira novamente outro valor.');
+        exibirMensagem('erro', 'O valor do campo "Quantidade de números" não pode ser 0 ou negativo, insira novamente outro valor');
         document.getElementById('quantidade').value = '';
         return false;
     }
     if (valorDeEhPermitido) {
-        alert('O valor do campo "de" não pode ser 0 ou negativo, insira novamente outro valor.');
+        exibirMensagem('erro','O valor do campo "Do número" não pode ser 0 ou negativo, insira novamente outro valor.');
         document.getElementById('de').value = '';
         return false;
     }
     if (valorAteEhPermitido) {
-        alert('O valor do campo quantidade não pode ser 0 ou negativo, insira novamente outro valor.');
+        exibirMensagem('erro', 'O valor do campo "Até o número" não pode ser 0 ou negativo, insira novamente outro valor.');
         document.getElementById('ate').value = '';
         return false;
     }
@@ -174,9 +166,7 @@ function validarValorInteiroDosCampos() {
  * 
  * Se do campo "ate" for menor do que o campo "de" ele para o sorteio e informa um aviso na tela.
  *
- * Parâmetros: Não se aplica.
- *
- * Retorno: true se válido, false caso contrário
+ * @returns {boolean} true se válido, false caso contrário
  */
 function validarInversaoDosValores() {
     // Atribui os valores dos campos nas variaveis:
@@ -184,7 +174,7 @@ function validarInversaoDosValores() {
     let valorDoCampoAte = parseInt(document.getElementById('ate').value);
     
     if (valorDoCampoAte < valorDoCampoDe) {
-        alert(`O valor no campo "Até" não pode ser menor do que o valor do campo "De". Por favor insira um valor maior do que ${valorDoCampoDe}.`);
+        exibirMensagem('erro', `O valor no campo "Até o número" não pode ser menor do que o valor do campo "Do número". Por favor insira um valor maior do que ${valorDoCampoDe}.`);
         document.getElementById('ate').value = "";
         return false;
     }
@@ -197,10 +187,8 @@ function validarInversaoDosValores() {
  * A função irá validar se o intervalo do valores "de" e "ate" é menor ou igual ao da "quantidade",
  * 
  * Se quantidade for maior que o intervalo informado, ele para o sorteio e informa um aviso na tela.
- *
- * Parâmetros: Não se aplica.
- *
- * Retorno: true se válido, false caso contrário
+ * 
+ * @returns {boolean} true se válido, false caso contrário
  */
 function validarQuantidadeXIntervaloDeSorteio() {
     // Atribui os valores dos campos nas variaveis:
@@ -210,11 +198,25 @@ function validarQuantidadeXIntervaloDeSorteio() {
     let intervalo = valorDoCampoAte - valorDoCampoDe + 1;
     
     if (valorQuantidade > intervalo) {
-        alert(`O campo quantidade é maior que o intervalo informado.\n`  +
-             `Informar a quantidade desejada até ${intervalo},\n` + 
-             `Ou informar no campo Até um valor superior.`);
+        exibirMensagem('erro', `A quantidade excede o intervalo permitido. Informe até ${intervalo} ou aumente o valor do campo "Até o número".`);
         return false;
     }
 
     return true;
+}
+
+/** Descrição da função:
+ * 
+ * Altera o conteúdo textual e a classe CSS de um elemento HTML.
+ * 
+ * PARÂMETROS:
+ *
+ * @param {string} classe Classe CSS que será aplicada ao elemento.
+ * @param {string} texto Mensagem que será exibida ao usuário.
+ * @returns {void} Não há retorno.
+ */
+function exibirMensagem(classe, texto) {
+    const campo = document.getElementById('resultado');
+    campo.textContent = texto;
+    campo.className = classe;
 }
